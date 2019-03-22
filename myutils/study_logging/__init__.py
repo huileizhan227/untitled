29f013f6275e8414e60fa4b60e30ba05203c76e1
huileizhan227 <374826581@qq.com>
2019-03-22 17:59:24 +0800

    add study_logging files to myutils

---
+++ myutils/study_logging/__init__.py
@@ -0,0 +1,5 @@
+#！/user/bin/env pythone2.7
+#! -*- coding:utf-8 -*-
+#! @Time : 2018/11/2 17:09
+#! @Auther : Yu Kunjiang
+#! @File : __init__.py.py
---
+++ myutils/study_logging/test_logging.py
@@ -0,0 +1,28 @@
+#！/user/bin/env pythone2.7
+#! -*- coding:utf-8 -*-
+#! @Time : 2018/11/2 16:52
+#! @Auther : Yu Kunjiang
+#! @File : test_logging.py
+
+import logging
+import logging.config
+'''
+日志文件保存所有 debug 及其以上级别的日志，每条日志中要有打印日志的时间，日志的级别和日志的内容
+'''
+# 方法一：利用logging.basicConfig来配置
+logging.basicConfig(
+    level= logging.DEBUG,
+    #format = '%(asctime)s %(levelname)-5.5s [%(name)s:%(lineno)s] %(message)s',
+    format = '[%(asctime)s] %(levelname)-5.5s [%(funcName)s: %(filename)s, %(lineno)d] %(message)s',
+    filename= "test.log",
+)
+
+# # 方法二：通过读取conf文件来配置
+# logging.config.fileConfig('test_logging.conf')
+
+#追加输出到test.log文件中
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warn message')
+logging.error('error message')
+logging.critical('critical message')
