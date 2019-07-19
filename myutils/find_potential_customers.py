935b2d4b985d70d0652502ebebafec12cbae3b55
huileizhan227 <374826581@qq.com>
2019-07-19 16:20:22 +0800

    modify find_potential_customers(add another method)

--- myutils/find_potential_customers.py
+++ myutils/find_potential_customers.py
@@ -4,6 +4,7 @@
 #! @Auther : Yu Kunjiang
 #! @File : find_potential_customers.py
 
+# 方法一：利用新建__hash__和__eq__方法来实现
 class VisitRecord():
     '''
     旅游记录
@@ -26,6 +27,27 @@ class VisitRecord():
             return True
         return False
 
+def find_potential_customers():
+    # 返回类型：VisitRecord
+    return set(VisitRecord(**r) for r in users_visited_phuket) - \
+           set(VisitRecord(**r) for r in users_visited_nz)
+
+# 方法二： 利用dataclasses 模块
+# from dataclasses import field, dataclass
+#
+# @dataclass(unsafe_hash=True)
+# class VisitRecordDC:
+#     first_name: str
+#     last_name: str
+#     phone_number: str
+#     # 跳过“访问时间”字段，不作为任何对比条件
+#     date_visited: str = field(hash=False, compare=False)
+#
+# def find_potential_customersDC():
+#     # 返回类型：VisitRecord
+#     return set(VisitRecordDC(**r) for r in users_visited_phuket) - \
+#            set(VisitRecordDC(**r) for r in users_visited_nz)
+
 # 去过普吉岛的人员数据
 users_visited_phuket = [
     {"first_name": "Sirena", "last_name": "Gross", "phone_number": "650-568-0388", "date_visited": "2018-03-14"},
@@ -39,10 +61,7 @@ users_visited_nz = [
     {"first_name": "James", "last_name": "Ashcraft", "phone_number": "412-334-4380", "date_visited": "2014-09-16"}
 ]
 
-def find_potential_customers():
-    # 返回类型：VisitRecord
-    return set(VisitRecord(**r) for r in users_visited_phuket) - \
-        set(VisitRecord(**r) for r in users_visited_nz)
+
 customers = find_potential_customers()
 for c in customers:
     print(c.first_name, c.last_name, c.phone_number, c.date_visited)
